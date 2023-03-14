0171cc6268298fa1005d448e49e8c561d81be03c
DaniCaba09 <dcaba.sanchez@gmail.com>
2023-03-14 18:02:22 +0100

    Programacion

---
+++ DanielCaba_3Trimestre/src/Comenzar.java
@@ -0,0 +1,59 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.UIManager;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+
+public class Comenzar extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Comenzar frame = new Comenzar();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Comenzar() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 102, 102));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ESTA ES LA FUCKING WEB ");
+		lblNewLabel.setForeground(new Color(255, 255, 51));
+		lblNewLabel.setBackground(Color.BLUE);
+		lblNewLabel.setFont(new Font("Malgun Gothic", Font.BOLD, 28));
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setBounds(10, 11, 414, 239);
+		contentPane.add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(242, 16, 1, 1);
+		contentPane.add(panel);
+		panel.setLayout(null);
+	}
+
+}
